22a2faf60a76a810cfc3419621e863317016f159
ta9446955-cpu <ta9446955@gmail.com>
2026-06-24 23:20:24 -0400

    Delete Default.py

--- Default.py
+++
@@ -1,8 +0,0 @@
-# In list_channels():
-for ch in channels:
-    real_url = client.get_stream_url(ch["id"])
-    if not real_url:
-        continue  # skip channels without working streams
-    ch["stream_url"] = real_url
-    # ... rest of the loop
-  
